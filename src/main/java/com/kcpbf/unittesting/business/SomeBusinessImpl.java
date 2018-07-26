f55a84cfb88f75ca5a0cdf9139da3f7cfd246895
Katia <katiacirlene@bitbucket.org>
2018-07-26 18:47:45 -0300

    step 02 - writing unit test for a simple business service

---
+++ src/main/java/com/kcpbf/unittesting/business/SomeBusinessImpl.java
@@ -0,0 +1,15 @@
+package com.kcpbf.unittesting.business;
+
+public class SomeBusinessImpl {
+
+	public int calculateSum(int[] data) {
+		int sum = 0;
+
+		for (int value : data) {
+			sum += value;
+		}
+
+		return sum;
+	}
+
+}
