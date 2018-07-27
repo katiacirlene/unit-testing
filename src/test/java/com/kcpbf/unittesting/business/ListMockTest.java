732e8bd58ca87088b2f8a64859bc18793f635d95
Katia <katiacirlene@bitbucket.org>
2018-07-27 11:30:52 -0300

    step 14 - introduction to Spy
    A mock does not retain the behavior (code) of the original class!
    
    A spy, by default, retains behavior (code) of the original class!

--- src/test/java/com/kcpbf/unittesting/business/ListMockTest.java
+++ src/test/java/com/kcpbf/unittesting/business/ListMockTest.java
@@ -7,10 +7,12 @@ import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -109,5 +111,45 @@ public class ListMockTest {
 		assertEquals("SomeString1", allValues.get(0));
 		assertEquals("SomeString2", allValues.get(1));
 	}	
+	
+	@Test
+	public void mocking() {
+		ArrayList arrayListMock = mock(ArrayList.class);
+		System.out.println(arrayListMock.get(0));	// null
+		System.out.println(arrayListMock.size()); 	// 0
+		
+		arrayListMock.add("Test1");
+		arrayListMock.add("Test2");
+		
+		System.out.println(arrayListMock.size());	// 0
+		
+		when(arrayListMock.size()).thenReturn(5);
+		
+		System.out.println(arrayListMock.size());	// 5
+	}
+	
+	@Test
+	public void spying() {
+		ArrayList arrayListSpy = spy(ArrayList.class);
+		arrayListSpy.add("Test0");
+		
+		System.out.println(arrayListSpy.get(0));	// Test0
+		System.out.println(arrayListSpy.size());		// 1
+		
+		arrayListSpy.add("Test1");
+		arrayListSpy.add("Test2");
+		
+		System.out.println(arrayListSpy.size());		// 3
+		
+		when(arrayListSpy.size()).thenReturn(5);
+		
+		System.out.println(arrayListSpy.size());		// 5
+		
+		arrayListSpy.add("Test3");
+		
+		System.out.println(arrayListSpy.size());		// 5
+		
+		verify(arrayListSpy).add("Test3");
+	}
 	
 }
