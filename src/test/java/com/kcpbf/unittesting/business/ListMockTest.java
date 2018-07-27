4b76d0f1fae97dbfcd284ff9a8f1a7a6fb9aaf99
Katia <katiacirlene@bitbucket.org>
2018-07-26 21:09:21 -0300

    step 11 - Mockito tips - verify method calls
    this verification is important in scenarios where a value is not returned back.

--- src/test/java/com/kcpbf/unittesting/business/ListMockTest.java
+++ src/test/java/com/kcpbf/unittesting/business/ListMockTest.java
@@ -2,7 +2,13 @@ package com.kcpbf.unittesting.business;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -11,7 +17,7 @@ import org.junit.Test;
 
 public class ListMockTest {
 	
-	private List mock = mock(List.class);
+	private List<String> mock = mock(List.class);
 
 	@Test
 	public void size_basic() {
@@ -48,6 +54,27 @@ public class ListMockTest {
 			
 		assertEquals("in28Minutes", mock.get(0));
 		assertEquals("in28Minutes", mock.get(1));
-	}	
+	}
+	
+	@Test
+	public void verificationBasics() {
+		
+		// System Under Test (SUT)
+		String value1 = mock.get(0);
+		String value2 = mock.get(1);
+		
+		// Verify
+		verify(mock).get(0);
+		
+		verify(mock, times(2)).get(anyInt());
+		
+		verify(mock, atLeast(1)).get(anyInt());
+		
+		verify(mock, atLeastOnce()).get(anyInt());
+		
+		verify(mock, atMost(2)).get(anyInt());
+		
+		verify(mock, never()).get(2);
+	}
 	
 }
