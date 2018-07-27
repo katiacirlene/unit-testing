5393c37902986d5f68ea245aa8146dfac25a4f4b
Katia <katiacirlene@bitbucket.org>
2018-07-27 18:48:17 -0300

    section 2 - step 05 - unit testing ItemController and basic JSON assertions

--- src/main/java/com/kcpbf/unittesting/controller/ItemController.java
+++ src/main/java/com/kcpbf/unittesting/controller/ItemController.java
@@ -10,7 +10,7 @@ public class ItemController {
 
 	@GetMapping("/dummy-item")
 	public Item dummyItem() {
-		return new Item(1, "Ball", 10, 1000);
+		return new Item(1, "Ball", 10, 100);
 	}
 	
 }
---
+++ src/test/java/com/kcpbf/unittesting/controller/ItemControllerTest.java
@@ -0,0 +1,41 @@
+package com.kcpbf.unittesting.controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = ItemController.class)
+public class ItemControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void dummyItem_basic() throws Exception {
+
+		RequestBuilder request = MockMvcRequestBuilders
+				.get("/dummy-item")
+				.accept(MediaType.APPLICATION_JSON);
+		
+		MvcResult result = mockMvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().json("{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}"))
+				.andReturn();
+		
+		// verify "Hello World"
+		//assertEquals("Hello World", result.getResponse().getContentAsString());
+	}
+
+}
