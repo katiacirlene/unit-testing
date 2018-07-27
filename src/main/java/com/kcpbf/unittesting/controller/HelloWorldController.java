33c9afc511dc3b2d027aee218839782681405c2d
Katia <katiacirlene@bitbucket.org>
2018-07-27 18:00:43 -0300

    section 2 - step 02 - using Mock Mvc to test HelloWorldController

--- src/main/java/com/kcpbf/unittesting/controller/HelloWorldController.java
+++ src/main/java/com/kcpbf/unittesting/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.kcpbf.unittesting;
+package com.kcpbf.unittesting.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
