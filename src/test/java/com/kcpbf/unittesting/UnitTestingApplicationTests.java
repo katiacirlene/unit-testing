c8f54df48481d5b3a7f685eca17163b096eca4fd
Katia <katiacirlene@bitbucket.org>
2018-07-29 18:02:51 -0300

    section 2 - step 24 - measuring test coverage with eclipse
    while running Test Coverage Tool on Eclipse I found this error, there was an whitespace between "classpath" and the fileName; the file was not being found because of this whitespace and an exception was being thrown.

--- src/test/java/com/kcpbf/unittesting/UnitTestingApplicationTests.java
+++ src/test/java/com/kcpbf/unittesting/UnitTestingApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@TestPropertySource(locations= {"classpath: test-configuration.properties"})
+@TestPropertySource(locations= {"classpath:test-configuration.properties"})
 public class UnitTestingApplicationTests {
 
 	@Test
